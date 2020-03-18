5bd3775b0babb36ebdb2ff7e7456dbb344207c1c
kong850539619 <38160715+kong850539619@users.noreply.github.com>
2020-03-18 14:49:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,14 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.5.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+#移除不用软件包
+rm -rf k3screenctrl package/lean
+#添加额外软件包
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
+git clone https://github.com/lwz322/k3screenctrl.git package/k3/k3screenctrl
+git clone https://github.com/lwz322/k3screenctrl_build.git package/k3/k3screenctrl_build
+git clone https://github.com/pvaret/rtl8192cu-fixes.git
+git clone https://github.com/P3TERX/xxx package/xxx
